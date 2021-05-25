704f5649aa724448a3d7fd0700860cb79bfc6614
johyeon yoon <yds03080@naver.com>
2021-05-25 13:16:14 +0900

    Initial commit

---
+++ SocialLogin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocialLogin
+//
+//  Created by Johyeon Yoon on 2021/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
