0c37999d6aad0ae1fa8e295b25f16f2e34bf1cf0
johyeon yoon <yds03080@naver.com>
2021-05-26 12:39:21 +0900

    Implement Bringing UserInfo in App

--- SocialLogin/ViewController.swift
+++ SocialLogin/ViewController.swift
@@ -18,33 +18,62 @@ class ViewController: UIViewController {
     
     @IBAction func touchUpKakaoLogin(_ sender : Any) {
 //        // 카카오톡 설치여부 확인
-//        if (UserApi.isKakaoTalkLoginAvailable()) {
-//            UserApi.shared.loginWithKakaoTalk {(oauthToken, error) in
-//                if let error = error {
-//                    // 예외처리
-//                    print(error)
-//                }
-//                else {
-//                    print("loginWithKakaoTalk() success.")
-//
-//                    //do something
-//                    _ = oauthToken
-//                }
-//            }
-//        }
-        UserApi.shared.loginWithKakaoAccount {(oauthToken, error) in
-            if let error = error {
-                print(error)
+        if (UserApi.isKakaoTalkLoginAvailable()) {
+            UserApi.shared.loginWithKakaoTalk {(oauthToken, error) in
+                if let error = error {
+                    // 예외처리
+                    print(error)
+                }
+                else {
+                    print("loginWithKakaoTalk() success.")
+                    _ = oauthToken
+                    // 액세스토큰
+                    let accessToken = oauthToken?.accessToken
+                    self.setUserInfo()
+                }
             }
-            else {
-                print("loginWithKakaoTalk() success.")
-               //
-               //                    //do something
-                _ = oauthToken
+        }
+        // 카카오톡 미설치된 경우
+        else {
+            UserApi.shared.loginWithKakaoAccount {(oauthToken, error) in
+                if let error = error {
+                    print(error)
+                }
+                else {
+                    print("loginWithKakaoAccount() success.")
+                    //do something
+                    _ = oauthToken
+                    let accessToken = oauthToken?.accessToken
+                    self.setUserInfo()
+                
+                }
+                
             }
             
         }
         
+    }
+    
+    @IBOutlet weak var infoLabel : UILabel!
+    @IBOutlet weak var emailLabel : UILabel!
+    @IBOutlet weak var profileImageView : UIImageView!
+    
+    func setUserInfo() {
+        UserApi.shared.me() { (user, error) in
+            if let error = error {
+                print(error)
+            }
+            else {
+                print("me() success")
+                _ = user
+                self.infoLabel.text = user?.kakaoAccount?.profile?.nickname
+                self.emailLabel.text = user?.kakaoAccount?.email
+                if let url = user?.kakaoAccount?.profile?.profileImageUrl,
+                   let data = try? Data(contentsOf: url){
+                    self.profileImageView.image = UIImage(data: data)
+                }
+            }
+        }
     }
     
 }
